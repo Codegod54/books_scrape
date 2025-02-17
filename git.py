ee4b1470a61ecdd1eed660594ea077d5876cd358
basant chaudhary <bmagar932@mail.com>
2025-02-17 12:56:57 +0545

    add git code

---
+++ git.py
@@ -0,0 +1,13 @@
+# install git 
+# creaate represotory in git hub 
+# go t o git bash
+# git config --global user.name "username"
+# git config --global user.email "email"
+
+# git init
+# git status => to check the status of the files
+# git diff => to check the difference between the files
+# git add . => to add all the files to the staging area
+# git commit -m "message" => to commit the files to the repository
+# copy paste git code from github and paste it in git bash
+
